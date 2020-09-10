893f3ac4ae0502dc56ab6c16eb98d89357e27c72
Lokendra Bohra <2017kucp1038@iiitkota.ac.in>
2020-09-10 17:03:18 +0530

    Stack

---
+++ DataStructures_Templates/Stack.java
@@ -0,0 +1,79 @@
+class StackOverFlowException extends Exception {
+    StackOverFlowException(String msg) {
+        super(msg);
+    }
+}
+class StackUnderFlowException extends Exception {
+    StackUnderFlowException(String msg){
+        super(msg);
+    }
+}
+
+public class Stack {
+    private int[] stack;
+    int top;
+    Stack(){
+        stack = new int[100];
+        top = 0;  //Top will be 0 if there are no elements. So if top = 0 and we want to pop, StackUnderFlow Exception is raised
+    }
+    Stack(int[] arr) throws StackOverFlowException{
+        stack = new int[100];
+        top = 0;
+        for (int e: arr)
+            this.push(e);
+    }
+    void push(int val) throws StackOverFlowException {
+        if (this.top < 100) {
+            this.stack[this.top++] = val;
+        }
+        else {
+            throw new StackOverFlowException("Stack is Full. Try Extending the cacpacity by extend() function");
+        }
+    }
+    int pop() throws StackUnderFlowException {
+        if (this.top <= 0){
+            throw new StackUnderFlowException("Stack is already Empty");
+        }
+        else {
+            int temp = this.stack[this.top--];
+            return temp;
+        }
+    }
+
+    int peek() throws StackUnderFlowException{
+        if (this.top > 0){
+            return this.stack[this.top-1];
+        }
+        else {
+            throw new StackUnderFlowException("Stack is Empty");
+        }
+    }
+
+    void print() throws StackUnderFlowException{
+        if (this.top <= 0)
+            throw new StackUnderFlowException("Stack is Empty");
+        else {
+            for(int e: this.stack)
+                System.out.print(e+' ');
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args){
+        Stack s1 = new Stack();
+        try {
+            //Try UnCommenting This Line
+            //s1.pop(); 
+            for(int i=0;i<100;i++)              
+                s1.push(i);
+            s1.print();
+        }
+        catch (StackUnderFlowException e) {
+            System.out.println(e.getMessage());
+        }
+        catch (StackOverFlowException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+}
