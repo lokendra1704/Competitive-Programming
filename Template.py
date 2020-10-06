c55ede6eb66f61b47b7c0990febf0f867e85a7d8
Lokendra Bohra <2017kucp1038@iiitkota.ac.in>
2020-10-06 16:40:28 +0530

    Added Template

---
+++ Template.py
@@ -0,0 +1,14 @@
+import sys
+from math import *
+from collections import *
+from functools import lru_cache, reduce
+from itertools import *
+cin = lambda: int(input())
+car = lambda:map(int,sys.stdin.readline().split())
+carr = lambda:list(map(int,sys.stdin.readline().split()))
+
+def fun():
+    pass
+
+for t in range(cin()):
+    pass
